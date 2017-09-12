265c567179170915d67c58ac6a99ca7716d667cb
Yidong <yidonglau@gmail.com>
2017-09-11 18:13:44 -0700

    make text blink correctly

--- public/js/blinkText.js
+++ public/js/blinkText.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var text = 'Check Out My Node.js Work!';
-$(text).hide().appendTo('h2').each(function(i) {
+var text = 'CHECK OUT MY NODE.JS WORK!';
+var blinkText = '<span>' + text.split('').join('</span><span>') + '</span>';
+$(blinkText).hide().appendTo('#blink').each(function(i) {
     $(this).delay(100 * i).fadeIn();
 });
