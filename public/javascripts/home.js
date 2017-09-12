be1999135884d99548722ce78cb966107307e822
Yidong <yidonglau@gmail.com>
2017-09-11 17:25:41 -0700

    add new designed home page

--- public/javascripts/home.js
+++
@@ -1,7 +0,0 @@
-var str = "CHECK OUT MY WORK";
-
-var spans = '<span>' + str.split("").join(' </span><span>') + '</span>';
-
-$(spans).hide().appendTo('body').each(function(i) {
-    $(this).delay(100 * i).fadeIn();
-});
